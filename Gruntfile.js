babe238c4e43b1b8d63a9d20931e3ae85926c6f4
Kazark <kazark@zoho.com>
2014-02-05 10:37:44 -0500

    Still working to get the Grunt environment set up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,2 +1,8 @@
 module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    grunt.registerTask('default', ['csslint', 'jshint', 'jasmine:all']);
 };
